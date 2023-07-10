46082f44652f487bb81ea6047db2532881b120da
adriancgarcia <adriancgarcia@gmail.com>
2023-07-10 00:25:34 -0500

    connected to mongoose.Show page not rendering.

--- models/seed.js
+++ models/seed.js
@@ -10,7 +10,7 @@ const allVenues = [
         name: "Bridge 410",
         img: "https://static.wixstatic.com/media/69f4f2_d1ac79dae94347edb71d06ab80911a76~mv2.jpg/v1/fill/w_632,h_862,al_c,q_85,usm_0.66_1.00_0.01/69f4f2_d1ac79dae94347edb71d06ab80911a76~mv2.jpg",
         location: "410 N. Paulina",
-        dateofWedding: "6/8/2021",
+        dateOfWedding: "06/08/2021",
         nameOfParty: "Garcia-Santos",
         venueWebsite: "www.bridge410.com",
         venueRating: 5,
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ require('dotenv').config(); // bring in our .env vars
 const express = require('express'); // web framework for node
 const morgan = require('morgan'); // logger for node
 const methodOverride = require('method-override'); // allows us to use PUT and DELETE methods
-const venue = require("./models/venue.js");
+const Venue = require("./models/venue.js");
 
 // express application
 const app = express();
@@ -21,71 +21,57 @@ app.use(express.urlencoded({extended: false}))
 // });
 
 // INDEX - GET
-app.get('/venues', (req, res) => {
-console.log(venue)
+app.get('/venues', async (req, res) => {
+    const venue = await Venue.find({});
 res.render('venue/index.ejs', {venue});
 });
 
 // NEW - GET
 app.get('/venues/new', (req, res) => {
-    res.render('venue/new.ejs', {venue})
+    res.render('venue/new.ejs', {Venue})
 });
 
 // DESTROY - DELETE
-app.delete("/venues/:id", (req, res) => {
-    const id = req.params.id
-    venue.splice(id, 1)
+app.delete("/venues/:id", async(req, res) => {
+    await Venue.findByIdAndDelete(req.params.body)
     res.redirect("/venues")
 });
 
 // UPDATE - PUT
-app.put("/venues/:id", (req, res) => {
-    const id = req.params.id;
-    venue[id] = {
-        name: req.body.name,
-        img: req.body.img,
-        location: req.body.location,
-        dateOfWedding: req.body.dateOfWedding,
-        nameOfParty: req.body.nameOfParty,
-        venueWebsite: req.body.venueWebsite,
-        venueRating: req.body.venueRating,
-        addToPortfolio: req.body.addToPortfolio,
-        comments: req.body.comments, 
+app.put("/venues/:id", async (req, res) => {
+    if(req.body.addToPortfolio === 'on') {
+        req.body.addToPortfolio = true;
+    }else {
+        req.body.addToPortfolio = false;
     }
+    await Venue.findByIdAndUpdate(req.params.id, req.body)
+
     res.redirect("/venues")
 });
 
 // CREATE POST
-app.post("/venues/", (req, res) => {
-    let newVenue = {
-        name: req.body.name,
-        img: req.body.img,
-        location: req.body.location,
-        dateOfWedding: req.body.dateOfWedding,
-        nameOfParty: req.body.nameOfParty,
-        venueWebsite: req.body.venueWebsite,
-        venueRating: req.body.venueRating,
-        addToPortfolio: req.body.addToPortfolio,
-        comments: req.body.comments, 
+app.post("/venues/", async (req, res) => {
+    if(req.body.addToPortfolio === 'on') {
+        req.body.addToPortfolio = true;
+    }else {
+        req.body.addToPortfolio = false;
     }
-    venue.push(newVenue)
+    await Venue.create(req.body)
     res.redirect("/venues")
 });
 
 // EDIT
-app.get("/venues/:id/edit", (req, res) => {
+app.get("/venues/:id/edit", async (req, res) => {
     const id = req.params.id;
-    const Venue = venue[id];
-    res.render("venue/edit.ejs", {Venue, id})
+    const venue = await Venue.findById(req.params.id);
+    res.render("venue/edit.ejs", {venue, id})
 });
 
 // SHOW 
-app.get("/venues/:id", (req, res) => {
-    const id = req.params.id;
-    const Venue = venue[id];
+app.get("/venues/:id", async (req, res) => {
+    const venue = await Venue.findById(req.params.id);
     res.render('venue/show.ejs', { Venue, id });
 });
-
 
 // Listen
 const PORT = process.env.PORT || 3000;
